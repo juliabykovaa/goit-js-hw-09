a508cfca2b126478e82abf1c41c7d15c5a65e79b
juliabykovaa <97822247+juliabykovaa@users.noreply.github.com>
2023-03-26 12:24:53 +0300

    switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startButton = document.querySelector('[data-start]')
+const stopButton = document.querySelector('[data-stop]')
 
+
+
+startButton.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = ${getRandomHexColor()};
+    }, 1000);
+    stopButton.disabled = false;
+    
+    startButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+    document.body.style.backgroundColor = "";
+    startButton.disabled = false;
+    stopButton.disabled = true;
+    clearInterval(timerId);
+
+});
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
